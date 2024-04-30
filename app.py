b675db0044c19c1592827427106529407ea424e4
Chandrashekhar <chandrashekhar130697@gmail.com>
2024-04-30 21:29:56 +0530

    model ran successfully

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_community.llms import ollama
+from langchain_community.llms import Ollama
 
 import os
 import streamlit as st
@@ -12,27 +12,24 @@ load_dotenv()
 # call enviroment
 ## Langsmith tracking
 os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
-os.environ["LANGCHAIN_TRACKING_V2"] = "true"
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","you are a helpful assistant. please provide responce to the user question"),
+        ("system","you are a helpful assistant. Please provide responce to the user question"),
         ("user","Question:{question}")
     ]
 )
 
 st.title("Langchain Demo with Llama3.")
-input_text = st.text_input("Search your query.")
-
-
-
+input_text = st.text_input("Search your query")
 
 # OpenAI LLM call
-llm = ollama(model="llama3")
+llm = Ollama(model="llama3")
 Output_Parser = StrOutputParser()
 
 # chain 
-chain = prompt|llm|StrOutputParser
+chain = prompt|llm|Output_Parser
 
 if input_text:
     st.write(chain.invoke({'question':input_text}))
